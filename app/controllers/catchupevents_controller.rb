b1148acbc3385d4691b96a36653008287606405e
Richa Dudhani <46418514+RichaDudhani@users.noreply.github.com>
2019-02-26 11:10:39 +1100

    Added new, create and destroy functionality

--- app/controllers/catchupevents_controller.rb
+++ app/controllers/catchupevents_controller.rb
@@ -8,4 +8,31 @@ class CatchupeventsController < ApplicationController
     @catchupevent = CatchUpEvent.find(params[:id])
     authorize @catchupevent
   end
+  
+  def new
+    @catchupevent = Catchupevent.new
+    authorize @catchupevent
+  end 
+  
+  def create
+    @catchupevent = Catchupevent.new(catchupevents_params)
+    authorize @catchupevent 
+    if @catchupevent.save
+      redirect_to my_catchups_path 
+    else 
+      render :new
+    end 
+  end 
+    
+  def destroy 
+    @catchupevent = Catchupevent.find(params[:id])
+    @catchupevent.destroy
+    redirect_to my_catchups_path 
+  end  
+  
+  private 
+  
+  def catchupevents_params
+    params.require(:catchupevent).permit(:category, :name, :location, :start_time, :end_time, :description, :capacity)
+  end 
 end

7df4b10e724ef54c0ef3cbb0b818008862a804c4
Jonas Japing <jonasjaping@gmail.com>
2019-02-27 16:27:54 +1100

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning DB"
-User.destroy_all
 Booking.destroy_all
 Catchupevent.destroy_all
+User.destroy_all
 
 puts "Creating two Users"
 jonas = User.create(
